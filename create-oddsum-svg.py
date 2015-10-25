5c6e5adc58fb1f14ee5a783b8a5184ea8dbca47a
Tom White <tom@cloudera.com>
2015-10-25 15:55:45 +0000

    Small improvements

--- create-oddsum-svg.py
+++ create-oddsum-svg.py
@@ -1,6 +1,6 @@
 import math
 
-n = 7
+n = 15
 width = n
 
 print """<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
@@ -11,12 +11,14 @@ print """<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/
 """ % (width, width)
 
 start = 0;
+gap = 0.05;
+small_gap = gap;
 for i in range(0, n):
   color = "magenta" if i % 2 == 0 else "blue"
-  print '<rect id="block" x="%s" y="%s" width="%s" height="%s" fill="%s" fill-opacity="0.5" />' % (i, i, 1 - 0.05, 1 - 0.05, color)
+  print '<rect id="block" x="%s" y="%s" width="%s" height="%s" fill="%s" fill-opacity="0.5" />' % (i, i, 1 - gap, 1 - gap, color)
   for j in range(0, i):
-    print '<rect id="block" x="%s" y="%s" width="%s" height="%s" fill="%s" fill-opacity="0.5" />' % (i, j, 1 - 0.05, 1 - 0.05, color)
-    print '<rect id="block" x="%s" y="%s" width="%s" height="%s" fill="%s" fill-opacity="0.5" />' % (j, i, 1 - 0.05, 1 - 0.05, color)
+    print '<rect id="block" x="%s" y="%s" width="%s" height="%s" fill="%s" fill-opacity="0.5" />' % (i, j, 1 - gap, 1 - small_gap, color)
+    print '<rect id="block" x="%s" y="%s" width="%s" height="%s" fill="%s" fill-opacity="0.5" />' % (j, i, 1 - small_gap, 1 - gap, color)
   print
 
 print """</svg>"""
